474ab8f70cf563ed75fcefb586df7450cafb11f3
agentinfraguard <agentinfraguard@gmail.com>
2018-01-08 17:59:37 +0530

    update agent
    update agent

--- scripts/agentUpdater.sh
+++ scripts/agentUpdater.sh
@@ -168,7 +168,7 @@ installAgent() {
      echo "station 7"
      export stopCommand="bash -c sleep 10 ;  sh $command ${stop};"
      echo $stopCommand
-     stopCommandOutput=$(bash -c sleep 10 ;  sh $command ${stop};)
+     stopCommandOutput=$(bash -c sleep 10 ;  sh $command ${stop};)&
      stopCommandOutput2=$stopCommandOutput;
      echo $stopCommandOutput
      echo $stopCommandOutput2
@@ -176,7 +176,7 @@ installAgent() {
      echo "station 9"
      echo "station 10"
      export startCommand="bash -c sleep 15 ;  sh $command ${start};"
-     startCommandOutput=$(bash -c sleep 15 ;  sh $command ${start};)
+     startCommandOutput=$(bash -c sleep 15 ;  sh $command ${start};)&
      startCommandOutput2=$startCommandOutput;
      echo $startCommandOutput
      echo $startCommandOutput2
